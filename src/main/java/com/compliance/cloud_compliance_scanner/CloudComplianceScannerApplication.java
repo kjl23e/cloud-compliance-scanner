8add686d118c6df01cb907628f1a971c602f2b7a
kennyjla <kennyj.la00@gmail.com>
2026-09-06 23:26:57 -0400

    Initialize Spring Boot base application skeleton

---
+++ src/main/java/com/compliance/cloud_compliance_scanner/CloudComplianceScannerApplication.java
@@ -0,0 +1,13 @@
+package com.compliance.cloud_compliance_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudComplianceScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudComplianceScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compliance/cloud_compliance_scanner/CloudComplianceScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compliance.cloud_compliance_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComplianceScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
